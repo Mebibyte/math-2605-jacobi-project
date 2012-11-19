4318825478a54951ad6c24900a26ef6fbc0b7165
nintendo424 <nintendo424@06c6862f-4f96-9123-1a8c-5e810e4d538e>
2012-11-19 01:15:38 +0000

    Made the GUI look better with Absolute Layout

--- src/GUI.java
+++ src/GUI.java
@@ -2,188 +2,190 @@ import javax.swing.*;
 import java.awt.GridBagLayout;
 import java.awt.GridBagConstraints;
 import java.awt.Insets;
+import java.awt.Font;
 
 
 public class GUI extends JFrame {
-	private JTextField Lbl00;
-	private JTextField textField_1;
-	private JTextField textField_2;
-	private JTextField textField_3;
-	private JTextField textField_4;
-	private JTextField textField_5;
-	private JTextField textField_6;
-	private JTextField textField_7;
-	private JTextField textField_8;
-	private JTextField textField_9;
-	private JTextField textField_10;
-	private JTextField textField_11;
-	private JTextField textField_12;
-	private JTextField textField_13;
-	private JTextField textField_14;
-	private JTextField textField_15;
-	private JTextField textField_16;
-	private JTextField textField_17;
-	private JTextField textField_18;
-	private JTextField textField_19;
-	private JTextField textField_20;
-	private JTextField textField_21;
-	private JTextField textField_22;
-	private JTextField textField_23;
-	private JTextField textField_24;
+	private JTextField lbl00;
+	private JTextField lbl01;
+	private JTextField lvl20;
+	private JTextField lbl03;
+	private JTextField lbl04;
+	private JTextField lbl10;
+	private JTextField lbl11;
+	private JTextField lbl12;
+	private JTextField lbl13;
+	private JTextField lbl14;
+	private JTextField lbl20;
+	private JTextField lbl21;
+	private JTextField lbl22;
+	private JTextField lbl23;
+	private JTextField lbl24;
+	private JTextField lbl30;
+	private JTextField lbl31;
+	private JTextField lbl32;
+	private JTextField lbl33;
+	private JTextField lbl34;
+	private JTextField lbl40;
+	private JTextField lbl41;
+	private JTextField lbl42;
+	private JTextField lbl43;
+	private JTextField lbl44;
+	
 	public GUI() {
 		setTitle("Jacobi Method");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		getContentPane().setLayout(null);
 		
-		Lbl00 = new JTextField();
-		Lbl00.setBounds(41, 22, 50, 25);
-		Lbl00.setText("0,0");
-		getContentPane().add(Lbl00);
-		Lbl00.setColumns(10);
+		lbl00 = new JTextField();
+		lbl00.setBounds(61, 53, 50, 25);
+		lbl00.setText("0,0");
+		getContentPane().add(lbl00);
+		lbl00.setColumns(10);
 		
-		textField_1 = new JTextField();
-		textField_1.setBounds(108, 22, 50, 25);
-		textField_1.setText("0,1");
-		getContentPane().add(textField_1);
-		textField_1.setColumns(10);
+		lbl01 = new JTextField();
+		lbl01.setBounds(123, 53, 50, 25);
+		lbl01.setText("0,1");
+		getContentPane().add(lbl01);
+		lbl01.setColumns(10);
 		
-		textField_2 = new JTextField();
-		textField_2.setBounds(169, 22, 50, 25);
-		textField_2.setText("0,2");
-		getContentPane().add(textField_2);
-		textField_2.setColumns(10);
+		lvl20 = new JTextField();
+		lvl20.setBounds(185, 53, 50, 25);
+		lvl20.setText("0,2");
+		getContentPane().add(lvl20);
+		lvl20.setColumns(10);
 		
-		textField_3 = new JTextField();
-		textField_3.setBounds(231, 22, 50, 25);
-		textField_3.setText("0,3");
-		getContentPane().add(textField_3);
-		textField_3.setColumns(10);
+		lbl03 = new JTextField();
+		lbl03.setBounds(247, 53, 50, 25);
+		lbl03.setText("0,3");
+		getContentPane().add(lbl03);
+		lbl03.setColumns(10);
 		
-		textField_4 = new JTextField();
-		textField_4.setBounds(296, 22, 50, 25);
-		textField_4.setText("0,4");
-		getContentPane().add(textField_4);
-		textField_4.setColumns(10);
+		lbl04 = new JTextField();
+		lbl04.setBounds(309, 53, 50, 25);
+		lbl04.setText("0,4");
+		getContentPane().add(lbl04);
+		lbl04.setColumns(10);
 		
-		textField_5 = new JTextField();
-		textField_5.setBounds(41, 57, 50, 25);
-		textField_5.setText("1,0");
-		getContentPane().add(textField_5);
-		textField_5.setColumns(10);
+		lbl10 = new JTextField();
+		lbl10.setBounds(61, 88, 50, 25);
+		lbl10.setText("1,0");
+		getContentPane().add(lbl10);
+		lbl10.setColumns(10);
 		
-		textField_6 = new JTextField();
-		textField_6.setBounds(115, 57, 49, 22);
-		textField_6.setText("1,1");
-		getContentPane().add(textField_6);
-		textField_6.setColumns(10);
+		lbl11 = new JTextField();
+		lbl11.setBounds(123, 88, 50, 25);
+		lbl11.setText("1,1");
+		getContentPane().add(lbl11);
+		lbl11.setColumns(10);
 		
-		textField_7 = new JTextField();
-		textField_7.setBounds(169, 57, 63, 22);
-		textField_7.setText("1,2");
-		getContentPane().add(textField_7);
-		textField_7.setColumns(10);
+		lbl12 = new JTextField();
+		lbl12.setBounds(185, 88, 50, 25);
+		lbl12.setText("1,2");
+		getContentPane().add(lbl12);
+		lbl12.setColumns(10);
 		
-		textField_8 = new JTextField();
-		textField_8.setBounds(237, 57, 55, 22);
-		textField_8.setText("1,3");
-		getContentPane().add(textField_8);
-		textField_8.setColumns(10);
+		lbl13 = new JTextField();
+		lbl13.setBounds(247, 88, 50, 25);
+		lbl13.setText("1,3");
+		getContentPane().add(lbl13);
+		lbl13.setColumns(10);
 		
-		textField_9 = new JTextField();
-		textField_9.setBounds(297, 57, 49, 22);
-		textField_9.setText("1,4");
-		getContentPane().add(textField_9);
-		textField_9.setColumns(10);
+		lbl14 = new JTextField();
+		lbl14.setBounds(309, 88, 50, 25);
+		lbl14.setText("1,4");
+		getContentPane().add(lbl14);
+		lbl14.setColumns(10);
 		
-		textField_10 = new JTextField();
-		textField_10.setBounds(42, 95, 50, 25);
-		textField_10.setText("2,0");
-		getContentPane().add(textField_10);
-		textField_10.setColumns(10);
+		lbl20 = new JTextField();
+		lbl20.setBounds(61, 126, 50, 25);
+		lbl20.setText("2,0");
+		getContentPane().add(lbl20);
+		lbl20.setColumns(10);
 		
-		textField_11 = new JTextField();
-		textField_11.setBounds(115, 84, 49, 22);
-		textField_11.setText("2,1");
-		getContentPane().add(textField_11);
-		textField_11.setColumns(10);
+		lbl21 = new JTextField();
+		lbl21.setBounds(123, 126, 50, 25);
+		lbl21.setText("2,1");
+		getContentPane().add(lbl21);
+		lbl21.setColumns(10);
 		
-		textField_12 = new JTextField();
-		textField_12.setBounds(169, 84, 63, 22);
-		textField_12.setText("2,2");
-		getContentPane().add(textField_12);
-		textField_12.setColumns(10);
+		lbl22 = new JTextField();
+		lbl22.setBounds(185, 126, 50, 25);
+		lbl22.setText("2,2");
+		getContentPane().add(lbl22);
+		lbl22.setColumns(10);
 		
-		textField_13 = new JTextField();
-		textField_13.setBounds(237, 84, 55, 22);
-		textField_13.setText("2,3");
-		getContentPane().add(textField_13);
-		textField_13.setColumns(10);
+		lbl23 = new JTextField();
+		lbl23.setBounds(247, 126, 50, 25);
+		lbl23.setText("2,3");
+		getContentPane().add(lbl23);
+		lbl23.setColumns(10);
 		
-		textField_14 = new JTextField();
-		textField_14.setBounds(297, 84, 49, 22);
-		textField_14.setText("2,4");
-		getContentPane().add(textField_14);
-		textField_14.setColumns(10);
+		lbl24 = new JTextField();
+		lbl24.setBounds(309, 126, 50, 25);
+		lbl24.setText("2,4");
+		getContentPane().add(lbl24);
+		lbl24.setColumns(10);
 		
-		textField_15 = new JTextField();
-		textField_15.setBounds(41, 133, 50, 25);
-		textField_15.setText("3,0");
-		getContentPane().add(textField_15);
-		textField_15.setColumns(10);
+		lbl30 = new JTextField();
+		lbl30.setBounds(61, 164, 50, 25);
+		lbl30.setText("3,0");
+		getContentPane().add(lbl30);
+		lbl30.setColumns(10);
 		
-		textField_16 = new JTextField();
-		textField_16.setBounds(115, 111, 49, 22);
-		textField_16.setText("3,1");
-		getContentPane().add(textField_16);
-		textField_16.setColumns(10);
+		lbl31 = new JTextField();
+		lbl31.setBounds(123, 164, 50, 25);
+		lbl31.setText("3,1");
+		getContentPane().add(lbl31);
+		lbl31.setColumns(10);
 		
-		textField_17 = new JTextField();
-		textField_17.setBounds(169, 111, 63, 22);
-		textField_17.setText("3,2");
-		getContentPane().add(textField_17);
-		textField_17.setColumns(10);
+		lbl32 = new JTextField();
+		lbl32.setBounds(185, 164, 50, 25);
+		lbl32.setText("3,2");
+		getContentPane().add(lbl32);
+		lbl32.setColumns(10);
 		
-		textField_18 = new JTextField();
-		textField_18.setBounds(237, 111, 55, 22);
-		textField_18.setText("3,3");
-		getContentPane().add(textField_18);
-		textField_18.setColumns(10);
+		lbl33 = new JTextField();
+		lbl33.setBounds(247, 164, 50, 25);
+		lbl33.setText("3,3");
+		getContentPane().add(lbl33);
+		lbl33.setColumns(10);
 		
-		textField_19 = new JTextField();
-		textField_19.setBounds(297, 111, 49, 22);
-		textField_19.setText("3,4");
-		getContentPane().add(textField_19);
-		textField_19.setColumns(10);
+		lbl34 = new JTextField();
+		lbl34.setBounds(309, 164, 50, 25);
+		lbl34.setText("3,4");
+		getContentPane().add(lbl34);
+		lbl34.setColumns(10);
 		
-		textField_20 = new JTextField();
-		textField_20.setBounds(41, 171, 50, 25);
-		textField_20.setText("4,0");
-		getContentPane().add(textField_20);
-		textField_20.setColumns(10);
+		lbl40 = new JTextField();
+		lbl40.setBounds(61, 202, 50, 25);
+		lbl40.setText("4,0");
+		getContentPane().add(lbl40);
+		lbl40.setColumns(10);
 		
-		textField_21 = new JTextField();
-		textField_21.setBounds(115, 138, 49, 22);
-		textField_21.setText("4,1");
-		getContentPane().add(textField_21);
-		textField_21.setColumns(10);
+		lbl41 = new JTextField();
+		lbl41.setBounds(123, 202, 50, 25);
+		lbl41.setText("4,1");
+		getContentPane().add(lbl41);
+		lbl41.setColumns(10);
 		
-		textField_22 = new JTextField();
-		textField_22.setBounds(169, 138, 63, 22);
-		textField_22.setText("4,2");
-		getContentPane().add(textField_22);
-		textField_22.setColumns(10);
+		lbl42 = new JTextField();
+		lbl42.setBounds(185, 202, 50, 25);
+		lbl42.setText("4,2");
+		getContentPane().add(lbl42);
+		lbl42.setColumns(10);
 		
-		textField_23 = new JTextField();
-		textField_23.setBounds(237, 138, 55, 22);
-		textField_23.setText("4,3");
-		getContentPane().add(textField_23);
-		textField_23.setColumns(10);
+		lbl43 = new JTextField();
+		lbl43.setBounds(247, 202, 50, 25);
+		lbl43.setText("4,3");
+		getContentPane().add(lbl43);
+		lbl43.setColumns(10);
 		
-		textField_24 = new JTextField();
-		textField_24.setBounds(297, 138, 49, 22);
-		textField_24.setText("4,4");
-		getContentPane().add(textField_24);
-		textField_24.setColumns(10);
+		lbl44 = new JTextField();
+		lbl44.setBounds(309, 202, 50, 25);
+		lbl44.setText("4,4");
+		getContentPane().add(lbl44);
+		lbl44.setColumns(10);
 		
 		JButton randomize = new JButton("Randomize");
 		randomize.setBounds(50, 315, 100, 30);
@@ -196,6 +198,10 @@ public class GUI extends JFrame {
 		JButton run = new JButton("Run");
 		run.setBounds(270, 315, 100, 30);
 		getContentPane().add(run);
+		
+		JLabel lblMatrix = new JLabel("Matrix:");
+		lblMatrix.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		lblMatrix.setBounds(179, 13, 56, 16);
+		getContentPane().add(lblMatrix);
 	}
-
 }
