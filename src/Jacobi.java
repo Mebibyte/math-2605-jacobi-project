f49172f9efb6d04b541263b58d0440e8ff90164d
duelgamer@gmail.com <duelgamer@gmail.com@06c6862f-4f96-9123-1a8c-5e810e4d538e>
2012-11-28 04:12:41 +0000

    Added radio buttons to switch between sorted & unsorted. Not sure I am doing unsorted correctly... Currently takes a lot of steps to complete. Also broke graph because I realized the points should be (step, ln(off)).

--- src/Jacobi.java
+++ src/Jacobi.java
@@ -1,4 +1,3 @@
-import java.util.Arrays;
 import java.util.Random;
 
 import Jama.Matrix;
@@ -35,41 +34,19 @@ public class Jacobi {
 				}
 			}
 		}
-		System.out.println("Max Offset at: (" + (maxI + 1) + ", " + (maxJ + 1) + ") with value: " + maxOff);
 		diagonalizeTwoByTwo(maxI, maxJ);
 	}
 	
 	public void diagonalizeTwoByTwo(int i, int j) {
 		double array2[][] = {{array[i][i], array[i][j]}, {array[j][i], array[j][j]}};
-		for (int x = 0; x < 2; x++) {
-			System.out.println(Arrays.toString(array2[x]));
-		}
 		Matrix A = new Matrix(array2);
 		Matrix U = A.eig().getV();
-		System.out.println("U:");
-		for (int x = 0; x < 2; x++) {
-			System.out.println(Arrays.toString(U.getArray()[x]));
-		}
-		System.out.println("D:");
-		Matrix D = A.eig().getD();
-		for (int x = 0; x < 2; x++) {
-			System.out.println(Arrays.toString(D.getArray()[x]));
-		}
 		Matrix G = new Matrix(new double[][]{{1, 0, 0, 0, 0}, {0, 1, 0, 0, 0}, {0, 0, 1, 0, 0}, {0, 0, 0, 1, 0}, {0, 0, 0, 0, 1}});
 		G.set(i, i, U.get(0, 0));
 		G.set(i, j, U.get(0, 1));
 		G.set(j, i, U.get(1, 0));
 		G.set(j, j, U.get(1, 1));
-		System.out.println("G: ");
-		for (int x = 0; x < 5; x++) {
-			System.out.println(Arrays.toString(G.getArray()[x]));
-		}
-		
 		Matrix B = G.transpose().times(new Matrix(array)).times(G);
-		System.out.println("New B: ");
-		for (int x = 0; x < 5; x++) {
-			System.out.println(Arrays.toString(B.getArray()[x]));
-		}
 		array = B.getArray();
 		for(int i2 = 0; i2 < array.length; i2++) {
 			for(int j2 = 0; j2 < array[0].length; j2++) {
@@ -93,14 +70,15 @@ public class Jacobi {
 	public double[][] getArray() {
 		return array;
 	}
-	
-    public static void main(String[] args) {
-    	Jacobi jacobi = new Jacobi();
-    	jacobi.randomize();
-    	for (int x = 0; x < 5; x++) {
-    		System.out.println(Arrays.toString(jacobi.array[x]));
-    	}
-    	jacobi.findLargestOffDiagonal();
-    	System.out.println(jacobi.calculateOffset());
-    }
+
+	public void findOffDiagonal() {
+		for (int i = 0; i < 5; i++) {
+			for (int j = i + 1; j < 5; j++) {
+				if (array[i][j] != 0) {
+					diagonalizeTwoByTwo(i, j);
+					return;
+				}
+			}
+		}
+	}
 }
